6fb218ec0c644c22a6baea2129cad09123746d98
BenLopez00 <lop.ben00@gmail.com>
2022-08-18 15:51:28 -0300

    Creadas los modelos de la base de datos
    Sequelize modelado

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -2,22 +2,58 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 
-/*- [ ] Raza con las siguientes propiedades:
-  - ID *
-  - Nombre *
-  - Altura *
-  - Peso *
-  - Años de vida
+/* Raza con las siguientes propiedades:
+    - ID *
+    - Nombre *
+    - Altura *
+    - Peso *
+	- Años de vida
 - [ ] Temperamento con las siguientes propiedades:
-  - ID
-  - Nombr
-  */
+	- ID
+	- Nombre
+    */
+
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    // defino el modelo
+    sequelize.define('dog', {
+    id:{
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
     },
-  });
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    height_min:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    weight_min:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    height_max:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    weight_max:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    life_span:{
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    image:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    createdInDB: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+    }
+    });
 };
