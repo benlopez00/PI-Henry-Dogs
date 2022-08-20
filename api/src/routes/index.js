fa1b5372e1cee4e9ed054af1e444eb10e49ee318
BenLopez00 <lop.ben00@gmail.com>
2022-08-20 19:04:34 -0300

    Agregadas funciones para obtener info de api y bd
    Funciones creadas para obtener la info de la api, de la base de datos y concatenar ambas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { Dog, Temperament } = require("../db");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +8,47 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+//Traer datos de la api
+const getDataApi = async () => {
+    const api = await fetch(
+        "https://api.thedogapi.com/v1/breeds"
+    );
+    const apiInfo = await api.data.map((e) => {
+        return {
+            id: e.id,
+            image: e.image.url,
+            name: e.name,
+            temperament: e.temperament,
+            weight: e.weight.imperial,
+            height: e.height.imperial,
+            life_span: e.life_span,
+        };
+    });
+    return apiInfo;
+};
+
+//Traer datos de la base de datos
+const getBd = async () => {
+    return await Dog.findAll({
+        include: {
+            model: Temperament,
+            attributes: ["name"],
+            through: {
+                attributes: [],
+            },
+        },
+    });
+};
+
+
+const getBreeds = async () => {
+    const apiInfo = await getDataApi();
+    const bdInfo = await getBd();
+    const allInfo = apiInfo.concat(bdInfo);
+    return allInfo;
+};
+
 
 
 module.exports = router;
