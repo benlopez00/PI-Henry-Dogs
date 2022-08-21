22fcacf95a6eec91d98ef4d562608b52970922d1
BenLopez00 <lop.ben00@gmail.com>
2022-08-20 21:12:14 -0300

    Agregado get temperaments

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -65,4 +65,26 @@ router.get("/dogs", async (req, res) => {
 });
 
 
+
+router.get('/temperaments', async (req, res)=>{
+    const temperamentsInfo = await fetch(`https://api.thedogapi.com/v1/breeds`)
+    const temperamentsInfoDos = await temperamentsInfo.json();
+    const temperamentsBd = temperamentsInfoDos.map((e) => e.temperament)//muchos arrelos
+        .toString()//Devuelve una cadena de caracteres (texto)
+        .trim()// eliminar espacios en blanco y tablulaciones
+        .split(/\s*,\s*/);//Esto imprime dos líneas; la primera línea imprime la cadena original, y la segunda línea imprime el array resultante.
+    
+    const filtrado = temperamentsBd.filter(e => e);
+    const filtradoEach =[... new Set (filtrado)];
+    filtradoEach.forEach(e =>{
+        Temperament.findOrCreate({// se fija si esta y si no esta lo crea 
+            where: {name: e},
+        })
+    })
+    const todosTemperaments =await Temperament.findAll();
+    res.json(todosTemperaments);
+})
+
+
+
 module.exports = router;
