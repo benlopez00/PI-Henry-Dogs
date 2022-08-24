3dfddf09fac67dc26a91ffebb2f669e82ac4c184
BenLopez00 <lop.ben00@gmail.com>
2022-08-24 01:40:38 -0300

    Ruta Dogs con param id

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -10,6 +10,24 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+/*  - [ X ] __GET /dogs__:
+        - Obtener un listado de las razas de perro
+        - Debe devolver solo los datos necesarios para la ruta principal
+    - [ ] __GET /dogs?name="..."__:
+        - Obtener un listado de las razas de perro que contengan la palabra ingresada como query parameter
+        - Si no existe ninguna raza de perro mostrar un mensaje adecuado
+    - [ X ] __GET /dogs/{idRaza}__:
+        - Obtener el detalle de una raza de perro en particular
+        - Debe traer solo los datos pedidos en la ruta de detalle de raza de perro
+    - Incluir los temperamentos asociados
+    - [ X ] __POST /dogs__:
+        - Recibe los datos recolectados desde el formulario controlado de la ruta de creación de raza de perro por body
+        - Crea una raza de perro en la base de datos relacionada con sus temperamentos
+    - [ X ] __GET /temperaments__:
+        - Obtener todos los temperamentos posibles
+        - En una primera instancia deberán obtenerlos desde la API externa y guardarlos en su propia base de datos y luego ya utilizarlos desde allí*/
+
+
 //Traer datos de la api
 const getDataApi = async () => {
     const api = await fetch("https://api.thedogapi.com/v1/breeds");
@@ -120,6 +138,24 @@ router.post('/dogs', async (req, res) => {
         res.status(200).send(createDog);
     } else {
         res.status(404).send('Data needed to proceed is missing');
+    }
+})
+
+
+
+router.get('/dogs/:idRaza', async (req, res) => {
+    /* http://localhost:3001/dogs/7 */
+    try {
+        const { idRaza } = req.params;
+        const allDogs = await getInfoDogs();
+        if (!idRaza) {
+            res.status(404).json("Couldn't find the name on DBase")
+        } else {
+            const dog = allDogs.find(dogui => dogui.id.toString() === idRaza);
+            res.status(200).json(dog)
+        }
+    } catch (error) {
+        res.status(404).send(error)
     }
 })
 
