5fc8410e2b251c2e2526ec9caec4ad2ab2bf094a
BenLopez00 <lop.ben00@gmail.com>
2022-08-26 00:02:02 -0300

    Arreglado el store
    era el configurestore mal instalado

--- client/src/redux/store/indexStore.js
+++ client/src/redux/store/indexStore.js
@@ -1,7 +1,8 @@
-import { configureStore, applyMiddleware } from "redux";
+import {applyMiddleware} from "redux";
+import { configureStore } from '@reduxjs/toolkit';
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from 'redux-thunk';
-import rootReducer from  '../reducer/index';
+import rootReducer from  '../reducer/indexReducer';
 
 
 export const store = configureStore(
